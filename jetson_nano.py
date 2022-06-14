b45159d45425410ce3caca1e76d4a6bc5ec8fe6e
Ben950128 <a0953883776@gmail.com>
2022-06-14 15:26:28 +0800

    modify model

--- jetson_nano.py
+++ jetson_nano.py
@@ -36,7 +36,7 @@ Trash = 6
 def classify_image(my_image):
     custom_image = image.load_img(my_image, target_size=(224, 224))
     img_array = image.img_to_array(custom_image)
-    processed_img = keras.applications.efficientnet_v2.preprocess_input(img_array).astype(np.float32)
+    processed_img = keras.applications.efficientnet.preprocess_input(img_array).astype(np.float32)
     swapped = np.moveaxis(processed_img, 0,1)
     arr4d = np.expand_dims(swapped, 0)
     new_prediction = class_convert(np.argmax(model.predict(arr4d), axis = -1))   #用class_convert進行文字分類
